c658b64d020610dfaf56420eab6ee1b597d6af75
betalantz <betalantz@gmail.com>
2024-02-02 15:59:05 -0800

    adds some implementation code

--- lib/debug.py
+++ lib/debug.py
@@ -1,3 +1,10 @@
 import ipdb
 
+from lib.computer import Computer
+
+mbp = Computer("apple", "macbook pro m1")
+air = Computer("apple", "mb air")
+air2 = Computer("apple", "mb air")
+acer = Computer("acer", "Nitro V 15")
+
 ipdb.set_trace()
